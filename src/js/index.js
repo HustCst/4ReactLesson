f4704adf2ce5322a9f77d667a5228e808792e8da
HustCst <835650053@qq.com>
2016-11-17 11:32:36 +0800

    once app

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Cst</h1>
+            </div>
+        )
+    }
+});
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('root')
+)
