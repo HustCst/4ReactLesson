324b307d7c6b3a150bc85c62ab0df5eea0ba86c9
HustCst <835650053@qq.com>
2016-11-17 13:30:00 +0800

    react-lesson1

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// 创建第一个节点 讲解jsx语法， jsx中如何使用js语法
+// 创建第一个节点 讲解jsx语法， jsx中如何使用js语法 style className
 // var App = React.createClass({
 //     render: function () {
 //         var styles = {
@@ -22,7 +22,7 @@ import ReactDom from 'react-dom';
 //     document.getElementById('root')
 // );
 
-// 动态添加属性 利用props
+// 动态添加属性 利用props 只读不能扩展 
 // var App = React.createClass({
 //     render: function () {
 //         var styles = {
@@ -47,7 +47,11 @@ import ReactDom from 'react-dom';
 //     document.getElementById('root')
 // );
 
-// 声明周期
+ 
+
+
+
+// state 声明周期
 
 // var App = React.createClass({
 //     getDefaultProps: function() {
@@ -60,7 +64,8 @@ import ReactDom from 'react-dom';
 //     getInitialState: function () {
 //         console.log('getInitialState');
 //         return {
-//             show: true
+//             show: true,
+//             name: 'cst'
 //         }
 //     },
 //     componentWillMount: function () {
@@ -68,11 +73,14 @@ import ReactDom from 'react-dom';
 //         var self = this;
 //         setTimeout(function () {
 //             console.log(self)
-//             self.setState({show: false})
+//             self.setState({show: false, name: 'jc'})
 //         }, 3000)
 //     },
 //     shouldComponentUpdate: function (a, b) {
 //         console.log('shouldComponentUpdate');
+//         // props只读不能扩展
+//         // this.props.name = 'lalalala';
+//         // console.log(this.props.name);
 //         // a: 将要改变的 props， b：将要改变的state
 //         console.log(a, b)
 //         // 返回false的时候 组件不会render   true的时候会render
@@ -93,6 +101,8 @@ import ReactDom from 'react-dom';
 //         var data = ['jc', 'cst', 'dg'];
 //         return (
 //             <div>
+//                 <h1>{this.props.name}</h1>
+//                 <h2>{this.state.name}</h2>
 //                 <ul>
 //                     {data.map(function (ele, index) {
 //                         return <li key={index + 100}>{ele}</li>
@@ -106,7 +116,7 @@ import ReactDom from 'react-dom';
 // ReactDom.render(
 //     <App name='cst'/>,
 //     document.getElementById('root')
-// )
+// );
 // // 移除组件
 // ReactDom.unmountComponentAtNode(
 //     <App/>,
@@ -116,5 +126,110 @@ import ReactDom from 'react-dom';
 
 
 
+// refs this
+
+// var App = React.createClass({
+//     getInitialState: function () {
+//         return {
+//             value: 'cst'
+//         }
+//     },
+//     onHandleChange: function () {
+//         this.setState({value:this.refs.inp.value});
+//     },
+//     render: function () {
+//         return (
+//             <div>
+//                 <input type='text' value={this.state.value} ref='inp' onChange={this.onHandleChange}/>
+//                 <span>{this.state.value}</span>
+//             </div>
+//         )
+//     }
+// })
 
 
+// ReactDom.render(<App/>, document.getElementById('root'));
+
+
+// //规定传输数据的类型
+// // Cst.propTypes = {
+// //     arr: React.PropTypes.object
+// // }
+// // Cst组件接受外部数据 arr = {arr1}等形式  
+
+// var App = React.createClass({
+//     render: function () {
+//         return (
+//             <div>            
+//                 <Cst arr={arr} />
+//                 <Cst arr={arr2} />
+//             </div>
+//         )
+//     }
+// })
+
+// Mask
+
+// var Mask = React.createClass({
+//     render: function () {
+//         var styles = {
+//             position: 'absolute',
+//             left: 0,
+//             right: 0,
+//             bottom: 0,
+//             top: 0,
+//             background: 'black',
+//             opacity: 0.5
+//         }
+//         return (
+//             <div style={styles}>
+//                 {this.props.children}
+//             </div>
+//         )   
+//     }
+// });
+
+// var Dialog = React.createClass({
+//     render: function () {
+//         var styles = {
+//             position: 'absolute',
+//             width: '100%',
+//             height: 200,
+//             background: 'orange',
+//             fontSize: 20,
+//             lineHeight: 200,
+//             textAlign: 'center',
+//             color: 'white'
+//         }
+//         if (this.props.show) {
+//             styles.display = 'block';
+//         }else {
+//             styles.display = 'none';
+//         }
+//         return (
+//             <div onClick={this.props.onClick}>
+//                 <Mask>
+//                     <div style={styles}>{this.props.name}</div>
+//                 </Mask>
+//             </div>
+//         )
+//     }
+// });
+// var App = React.createClass({
+//     getInitialState: function () {
+//         return {
+//             show: true,
+//         }
+//     },
+//     onHandleClick: function () {
+//         this.setState({show: false});
+//     },
+//     render: function () {
+//         return (
+//             <div>
+//                 <Dialog name={this.props.name} show={this.state.show} onClick={this.onHandleClick}/>
+//             </div>
+//         )
+//     }
+// }) 
+// ReactDom.render(<App name='cst'/>, document.getElementById('root'));
